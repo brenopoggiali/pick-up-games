8dee1c9e0591143509f61e38208e059882e57ecf
guilhermealbm <gui.miranda.13@gmail.com>
2019-06-19 11:52:33 -0300

    Fixing participa_de_grupo_de_pelada files

--- documentacao/Arquivos auxiliares/remove_col.py
+++ documentacao/Arquivos auxiliares/remove_col.py
@@ -9,12 +9,28 @@ data.sort_values("AtletaID", inplace = True)
 data.drop_duplicates(subset ="AtletaID", 
 					keep = 'first', inplace = True) 
 
-grupos = pd.read_csv("../data/times_final.csv")
-grupos["ClubeID"] = grupos["ClubeID"].str.replace(" ", "")
+data["ClubeID"] = data["ClubeID"].str.replace("Atlético-GO", "373")
+data["ClubeID"] = data["ClubeID"].str.replace("Atlético-MG", "282")
+data["ClubeID"] = data["ClubeID"].str.replace("Atlético-PR", "293")
+data["ClubeID"] = data["ClubeID"].str.replace("Avaí", "314")
+data["ClubeID"] = data["ClubeID"].str.replace("Bahia", "265")
+data["ClubeID"] = data["ClubeID"].str.replace("Botafogo", "263")
+data["ClubeID"] = data["ClubeID"].str.replace("Chapecoense", "315")
+data["ClubeID"] = data["ClubeID"].str.replace("Corinthians", "264")
+data["ClubeID"] = data["ClubeID"].str.replace("Coritiba", "294")
+data["ClubeID"] = data["ClubeID"].str.replace("Cruzeiro", "283")
+data["ClubeID"] = data["ClubeID"].str.replace("Figueirense", "316")
+data["ClubeID"] = data["ClubeID"].str.replace("Flamengo", "262")
+data["ClubeID"] = data["ClubeID"].str.replace("Fluminense", "266")
+data["ClubeID"] = data["ClubeID"].str.replace("Grêmio", "284")
+data["ClubeID"] = data["ClubeID"].str.replace("Internacional", "285")
+data["ClubeID"] = data["ClubeID"].str.replace("Palmeiras", "275")
+data["ClubeID"] = data["ClubeID"].str.replace("Ponte Preta", "303")
+data["ClubeID"] = data["ClubeID"].str.replace("Santa Cruz", "344")
+data["ClubeID"] = data["ClubeID"].str.replace("Santos", "277")
+data["ClubeID"] = data["ClubeID"].str.replace("São Paulo", "276")
+data["ClubeID"] = data["ClubeID"].str.replace("Sport", "292")
+data["ClubeID"] = data["ClubeID"].str.replace("Vasco", "267")
+data["ClubeID"] = data["ClubeID"].str.replace("Vitória", "287")
 
-final = pd.merge(data, grupos, on='ClubeID', how='inner')
-
-lista = ["AtletaID", "id_grupo_de_pelada"]
-final = final[lista]
-
-final.to_csv("../data/participa_de_grupo_de_pelada.csv", index=False) #Output file
+data.to_csv("../data/participa_de_grupo_de_pelada.csv", index=False) #Output file
