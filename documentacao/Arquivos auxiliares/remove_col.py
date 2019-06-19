1861bb408498de50b9cba772d343a035480beaa3
guilhermealbm <gui.miranda.13@gmail.com>
2019-06-19 01:39:56 -0300

    Adding right participa_de_grupo_de_pelada csv and sql

--- documentacao/Arquivos auxiliares/remove_col.py
+++ documentacao/Arquivos auxiliares/remove_col.py
@@ -1,5 +1,20 @@
 import pandas as pd
-f=pd.read_csv("2018/2018_agregados.csv") #Imput file
-keep_col = ["atletas.atleta_id","atletas.pontos_num","FC","FD","FF","FS","G","I","RB","CA","PE","A","SG","DD","FT","GS","CV","GC"] #kept columns
-new_f = f[keep_col]
-new_f.to_csv("scouts2018.csv", index=False) #Output file
+f=pd.read_csv("../data/dados_agregados_limpos.csv") #Imput file
+keep_col = ["AtletaID", "ClubeID"] #kept columns
+data = f[keep_col]
+# sorting by first name 
+data.sort_values("AtletaID", inplace = True) 
+
+# dropping ALL duplicte values 
+data.drop_duplicates(subset ="AtletaID", 
+					keep = 'first', inplace = True) 
+
+grupos = pd.read_csv("../data/times_final.csv")
+grupos["ClubeID"] = grupos["ClubeID"].str.replace(" ", "")
+
+final = pd.merge(data, grupos, on='ClubeID', how='inner')
+
+lista = ["AtletaID", "id_grupo_de_pelada"]
+final = final[lista]
+
+final.to_csv("../data/participa_de_grupo_de_pelada.csv", index=False) #Output file
