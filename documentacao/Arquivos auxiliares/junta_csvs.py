565150161d6b27d473d8f8d7239a8c652450e97e
guilhermealbm <gui.miranda.13@gmail.com>
2019-06-20 20:08:46 -0300

    Adding peladas files

--- documentacao/Arquivos auxiliares/junta_csvs.py
+++ documentacao/Arquivos auxiliares/junta_csvs.py
@@ -4,9 +4,10 @@ import os
 from datetime import datetime, timedelta
 
 grupos = pd.read_csv("../data/times_final.csv")
-times = grupos["id_grupo_de_pelada"]
+#times = grupos["id_grupo_de_pelada"]
 
-new_f = times
+lista = ["nome", "id_grupo_de_pelada"]
+new_f = grupos[lista]
 new_f.to_csv("../data/grupo_pelada_temp.csv", index=False) #Output file
 
 # current date and time
@@ -14,27 +15,44 @@ now = datetime.now()
 timestamp1 = datetime.timestamp(now)
 timestamp2 = datetime.timestamp(datetime.now() + timedelta(seconds=3600))
 
+all = []
 with open('../data/grupo_pelada_temp.csv','r') as csvinput:
     with open('../data/pelada.csv', 'w') as csvoutput:
         writer = csv.writer(csvoutput, lineterminator='\n')
         reader = csv.reader(csvinput)
 
-        all = []
         row = next(reader)
-        row.append("id_grupo_de_pelada")
         row.append("lugar")
         row.append("preco")
         row.append("inicio")
         row.append("fim")
+        row.append("id_grupo_de_pelada")
         all.append(row)
 
         for row in reader:
-            row.append("CEU")
+            row.append("Campo do " + row[0])
             row.append(4.50)
             row.append(timestamp1)
             row.append(timestamp2)
+            row.append(row[1])
             all.append(row)
 
         writer.writerows(all)
 
+with open('../data/pelada.csv','a') as fd: #Mexer nisso pra adicionar as linhas atuais *8
+    for linha in all:
+        fd.write(",".join(map(str, linha))+"\n")
+        fd.write(",".join(map(str, linha))+"\n")
+        fd.write(",".join(map(str, linha))+"\n")
+        fd.write(",".join(map(str, linha))+"\n")
+        fd.write(",".join(map(str, linha))+"\n")
+        fd.write(",".join(map(str, linha))+"\n")
+        fd.write(",".join(map(str, linha))+"\n")
+
 os.remove("../data/grupo_pelada_temp.csv")
+
+f=pd.read_csv("../data/pelada.csv") #Imput file
+keep_col = ["lugar", "preco", "inicio", "fim", "id_grupo_de_pelada"] #kept columns
+data = f[keep_col]
+data.sort_values("id_grupo_de_pelada", inplace = True) 
+data.to_csv("../data/pelada.csv", index=False)
